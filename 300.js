47a53cb037d4f80edd1a50e202ed247eb7627394
masiben <52550783+masiben@users.noreply.github.com>
2024-02-24 09:55:10 +0800

    Add files via upload

---
+++ 300.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '665e37fed26f1772175f8c18dd596e8f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/665e37fed26f1772175f8c18dd596e8f/invoke.js"></scr' + 'ipt>');
---
+++ 728.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'f65d42eac76613a199a73507afbd7b78',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/f65d42eac76613a199a73507afbd7b78/invoke.js"></scr' + 'ipt>');
